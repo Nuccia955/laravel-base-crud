8649f36c1ffd0a36a4a5a37e1cbca2bd3e4b803b
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-26 14:51:07 +0100

    show first instances of comics table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,55 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <section class="comics-archive">
-        comics list
+    <section class="comics-archive container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>
+                        ID
+                    </th>
+                    <th>
+                        Title
+                    </th>
+                    <th>
+                        Price
+                    </th>
+                    <th>
+                        Series
+                    </th>
+                    <th colspan="3">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($comics as $comic )
+                    <tr>
+                        <td>
+                            {{ $comic->id }}
+                        </td>
+                        <td>
+                            {{ $comic->title }}
+                        </td>
+                        <td>
+                            {{ $comic->price }}$
+                        </td>
+                        <td>
+                            {{ $comic->series }}
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-primary">Show details</a>
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-warning">Edit</a>
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </section>
 @endsection
